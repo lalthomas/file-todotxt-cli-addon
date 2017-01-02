d514a794e9d802cfbd9de62a8730990737c18777
Lal Thomas <lal.thomas.mail@gmail.com>
2017-01-02 22:28:55 +0530

    add run file

--- file run.bat
+++ file run.bat
@@ -1,6 +1,14 @@
-@echo OFF 	
+@echo OFF
 REM File : file run.bat 	
 REM Creation Date : 2017-01-02 	
 REM Author : Lal Thomas 	
 REM Original File : D:\Dropbox\project\20160222-file todotxt cli addon\file 	
-	
+
+%~d0
+cd "%~p0"
+set scriptFolderPathFull=%~dp0%
+set scriptFolderPath=%scriptFolderPathFull:~0,-1%
+pushd "%userProfile%\.todo.actions.d"
+echo "%scriptFolderPath%"
+mklink /d file "%scriptFolderPath%"
+popd
